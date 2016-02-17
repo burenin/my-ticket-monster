d118f4edecd361e3f896879941b28600ff002c7e
Andrej Burenin <andrejs.burenins@dukascopy.com>
2016-02-17 09:48:05 +0200

    base endpoint evolve

--- src/main/java/org/jboss/examples/ticketmonster/rest/BaseEntityService.java
+++ src/main/java/org/jboss/examples/ticketmonster/rest/BaseEntityService.java
@@ -13,6 +13,7 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -118,6 +119,27 @@ public abstract class BaseEntityService<T> {
 		Map<String, Long> result = new HashMap<>();
 		result.put("count", entityManager.createQuery(criteriaQuery).getSingleResult());
 		return result;
+	}
+	
+	/**
+	* <p>
+	* A method for retrieving individual entity instances.
+	* </p>
+	* @param id entity id
+	* @return
+	*/
+	@GET
+	@Path("/{id:[0-9][0-9]*}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public T getSingleInstance(@PathParam("id") Long id) {
+		final CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		final CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(entityClass);
+		Root<T> root = criteriaQuery.from(entityClass);
+		
+		Predicate condition = criteriaBuilder.equal(root.get("id"), id);
+		
+		criteriaQuery.select(criteriaBuilder.createQuery(entityClass).getSelection()).where(condition);
+		return entityManager.createQuery(criteriaQuery).getSingleResult();
 	}
 
 	private List<T> getAll(MultivaluedMap<String, String> queryParameters) {
